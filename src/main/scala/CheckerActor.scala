f3ae5fda8c5580a3bed53c2b94f7b13e3c671f06
Felix <felixjb@hotmail.fr>
2020-03-20 18:30:49 +0100

    beat and tick OK !

--- src/main/scala/CheckerActor.scala
+++ src/main/scala/CheckerActor.scala
@@ -13,64 +13,59 @@ case class CheckerTick () extends Tick
 
 class CheckerActor (val id:Int, val terminaux:List[Terminal], electionActor:ActorRef) extends Actor {
 
-     var time : Int = 200
+     var time : Int = 500
      val father = context.parent
 
      var nodesAlive:List[Int] = List()
+     var nodesAliveOld:List[Int] = List()
      var datesForChecking:List[Date] = List()
      var lastDate:Date = null
-
-     var nodesChecked: List[Int] = List()
 
      var leader : Int = 0
 
     def receive = {
 
          // Initialisation
-        case Start () => {
-            self ! CheckerTick
-            father ! Message("test!!!")
+        case Start => {
+          self ! CheckerTick
         }
 
         // A chaque fois qu'on recoit un Beat : on met a jour la liste des nodes
         case IsAlive (nodeId) => {
+//          father ! Message ("Node "+ nodeId + " is alive" )
           if (!nodesAlive.contains(nodeId)) {
-            nodesAlive:::List(nodeId)
-            father ! Message ("Node " + nodeId + " is born !")
+            nodesAlive = nodesAlive:::List(nodeId)
           }
-
+          if(!nodesAlive.contains(nodeId)) nodesAlive = nodesAlive:::List(nodeId)
         }
-//          father ! Message ("node " + nodeId + " is alive !")
 
         case IsAliveLeader (nodeId) => {
-          if(nodeId == this.id) father ! Message ("I'm the Leader !")
+//          if(nodeId == this.id) father ! Message ("I'm the Leader !")
+//          else father ! Message ("Node "+ nodeId + " is leader and is alive" )
           if (leader != nodeId) {
             leader = nodeId
             father ! LeaderChanged (nodeId)
           }
+          if(!nodesAlive.contains(nodeId)) nodesAlive = nodesAlive:::List(nodeId)
         }
-
-//          father ! Message ("node " + nodeId + " is Leader and is alive !")
 
 
         // A chaque fois qu'on recoit un CheckerTick : on verifie qui est mort ou pas
         // Objectif : lancer l'election si le leader est mort
         case CheckerTick => {
-
           context.system.scheduler.scheduleOnce(this.time milliseconds, self, CheckerTick)
 
-          father ! Message("Tick")
-          val toDelete: List[Int] = List()
-            for(i <- 0 to nodesAlive.length) {
-              if (!nodesChecked.contains(i)) {
-                if(i == leader) {
-                  father ! Message ("Node "+ i + " was leader but is dead !!!!!!!!!!!!!!!!!")
-                }
-                else father ! Message ("Node "+ i + " is dead !")
-                toDelete:::List(i)
+          for(i <- nodesAliveOld.indices) {
+            val current = nodesAliveOld(i)
+            if (!nodesAlive.contains(current)) {
+              if (current == leader) {
+                father ! Message ("Node " + current + " was leader but is dead !!!!!!!!!!!!!!!!!")
               }
+              else father ! Message ("Node " + current + " is dead !")
             }
-          nodesAlive = nodesAlive.filter(n => toDelete.contains(n))
+          }
+          nodesAliveOld = nodesAlive
+          nodesAlive = List()
         }
 
     }
